4e5a1e1576bb4102e32763c5ebb9b2ffd7fcfdd1
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2025-02-07 00:54:35 +0530

    UI improved

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login_page/', views.login_page, name="login_page"),
     path('signup_page/', views.signup_page, name="signup_page"),
     path('', views.home, name="home"),
-    path('/chatbot', views.chatbot_dashboard, name="chatbot_dashboard"),
+    path('chatbot/', views.chatbot_dashboard, name="chatbot_dashboard"),
 ]
