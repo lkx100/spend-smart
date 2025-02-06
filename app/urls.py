ab2557117f0f0bbb204211669c28df6f25a637e8
Lucky <lk5999950@gmail.com>
2025-02-06 19:29:14 +0530

    login & signup setup

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('login_page/', views.login_page, name="login_page"),
+    path('signup_page/', views.signup_page, name="signup_page"),
+    path('', views.home, name="home"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def signup_page(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('firstName')
+        lastName = request.POST.get('lastName')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        # filter the user from User model
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.info(request, f"{username} already taken!")
+            return redirect('login_page')
+        else:
+            user = User.objects.create(
+                first_name = first_name,
+                last_name = lastName,
+                username = username,
+                email = email,
+                # password can't be set directly, as it would not encrypt it & remain as raw str!
+            )
+            # Django's set_password method encrypts the password
+            user.set_password(password)
+            user.save()  # Save the user to the database
+            messages.info(request, f"Account created successfully")
+
+    return render(request, 'signup.html')
+
+def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if User.objects.all().filter(username=username).exists():
+            validUser = authenticate(username=username, password=password)
+            if validUser:
+                login(request, validUser)   # Add user to a session
+                return redirect("home")
+            else:
+                messages.info(request, f"Invalid Password, Try Again")
+        else:
+            messages.info(request, f"username \"{username}\" Not found")
+
+    return render(request, 'login.html')
+
+
+def home(request):
+    return render(request, 'home.html')
